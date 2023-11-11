4868498af76097da34d9bfa45ed95d7afb02f7f0
RJ Paraiso <103213491+ThisIs-RJP@users.noreply.github.com>
2023-11-11 23:30:22 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@
 
 import joker from './images/joker.png';
 import ayase from './images/ayaseIcon.jpg';
+import insta from './images/instagram.png';
+import linkedIn from './images/linkedIinWhite.png';
+import spotify from './images/spotify.png';
+import discord from './images/discord.png';
 
 // ### Important imports
 import './App.css';
+import React, { useState } from 'react';
+
+// ### Important functions
 
 function App() {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <div className="App">
 
@@ -30,9 +39,9 @@ function App() {
         </div>
         <div className='backgroundHalf2'>
         </div>
-
-
       </div>
+
+      {/* #### This is the Middle Box */}
       <div className='aboutMe'>
         <div className='meBox'>
           <img src={ayase} id="meIcon" />
@@ -40,11 +49,72 @@ function App() {
           <div id='redLine'></div>
           <div className='profileStuff'>
             Programmer<br></br>Front-End Developer
-
           </div>
+
+          <div className='socials'>
+            <a href="">
+              <img src={insta} className='icons' />
+            </a>
+            <a href="">
+              <img src={discord} className='icons' />
+            </a>
+            <a href="">
+              <img src={linkedIn} className='icons' />
+            </a>
+            <a href="">
+              <img src={spotify} className='icons' />
+            </a>
+          </div>
+
         </div>
 
+        {/* ### This is the first box */}
+
+        <div className='sideText'>
+          <h1>
+            <i>Welcome</i>
+          </h1>
+          <div className='buttonLine'>
+            <div className='cvButton'>
+              <button
+                className={`animated-button ${isHovered ? 'hovered' : ''}`}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+              >
+                My CV
+              </button>
+              {/* <a href=".files/CV_RJ_Paraiso.pdf">My CV</a> */}
+            </div>
+
+            <div className='cvButton'>
+              <button
+                className={`animated-button ${isHovered ? 'hovered' : ''}`}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+              >
+                Websites
+              </button>
+              {/* <a href=".files/CV_RJ_Paraiso.pdf">My CV</a> */}
+            </div>
+
+            <div className='cvButton'>
+              <button
+                className={`animated-button ${isHovered ? 'hovered' : ''}`}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+              >
+                Projects
+              </button>
+              {/* <a href=".files/CV_RJ_Paraiso.pdf">My CV</a> */}
+            </div>
+          </div>
+
+          <div className='description'>
+            Hi!<br></br> My name is RJ.<br></br> Please find your way around my website using the elements provided.
+          </div>
+        </div>
       </div>
+      {/* #### This is the end of the Middle Box */}
 
       {/* #### Dont touch this div */}
     </div>
