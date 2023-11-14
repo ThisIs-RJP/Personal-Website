7b855f325837b5a780823cb417b33ec0fdbd9eaa
RJ Paraiso <103213491+ThisIs-RJP@users.noreply.github.com>
2023-11-14 12:06:21 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ import React, { useState } from 'react';
 
 function App() {
   const [isHovered, setIsHovered] = useState(false);
+  const [isHovered2, setIsHovered2] = useState(false);
+  const [isHovered3, setIsHovered3] = useState(false);
 
   return (
     <div className="App">
@@ -34,87 +36,120 @@ function App() {
         </div>
       </ul>
       {/* ### THIS IS THE END OF MY NAVIGATION BAR ------------------------ */}
-      <div className='profileBackground'>
-        <div className='backgroundHalf'>
-        </div>
-        <div className='backgroundHalf2'>
-        </div>
-      </div>
 
-      {/* #### This is the Middle Box */}
-      <div className='aboutMe'>
-        <div className='meBox'>
-          <img src={ayase} id="meIcon" />
-          <span id='namePlace2'>RJ Paraiso</span>
-          <div id='redLine'></div>
-          <div className='profileStuff'>
-            Programmer<br></br>Front-End Developer
+      <div id="profileProfile">
+        <div className='profileBackground'>
+          <div className='backgroundHalf'>
           </div>
-
-          <div className='socials'>
-            <a href="">
-              <img src={insta} className='icons' />
-            </a>
-            <a href="">
-              <img src={discord} className='icons' />
-            </a>
-            <a href="">
-              <img src={linkedIn} className='icons' />
-            </a>
-            <a href="">
-              <img src={spotify} className='icons' />
-            </a>
+          <div className='backgroundHalf2'>
           </div>
-
         </div>
 
-        {/* ### This is the first box */}
-
-        <div className='sideText'>
-          <h1>
-            <i>Welcome</i>
-          </h1>
-          <div className='buttonLine'>
-            <div className='cvButton'>
-              <button
-                className={`animated-button ${isHovered ? 'hovered' : ''}`}
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-              >
-                My CV
-              </button>
-              {/* <a href=".files/CV_RJ_Paraiso.pdf">My CV</a> */}
+        {/* #### This is the Middle Box */}
+        <div className='aboutMe'>
+          <div className='meBox'>
+            <img src={ayase} id="meIcon" />
+            <span id='namePlace2'>RJ Paraiso</span>
+            <div id='redLine'></div>
+            <div className='profileStuff'>
+              Programmer<br></br>Front-End Developer
             </div>
 
-            <div className='cvButton'>
-              <button
-                className={`animated-button ${isHovered ? 'hovered' : ''}`}
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-              >
-                Websites
-              </button>
-              {/* <a href=".files/CV_RJ_Paraiso.pdf">My CV</a> */}
+            <div className='socials'>
+              <a href="">
+                <img src={insta} className='icons' />
+              </a>
+              <a href="">
+                <img src={discord} className='icons' />
+              </a>
+              <a href="">
+                <img src={linkedIn} className='icons' />
+              </a>
+              <a href="">
+                <img src={spotify} className='icons' />
+              </a>
             </div>
 
-            <div className='cvButton'>
-              <button
-                className={`animated-button ${isHovered ? 'hovered' : ''}`}
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-              >
-                Projects
-              </button>
-              {/* <a href=".files/CV_RJ_Paraiso.pdf">My CV</a> */}
-            </div>
           </div>
 
-          <div className='description'>
-            Hi!<br></br> My name is RJ.<br></br> Please find your way around my website using the elements provided.
+          {/* ### This is the first box */}
+
+          <div className='sideText'>
+            <h1>
+              <i>Welcome</i>
+            </h1>
+            <div className='buttonLine'>
+              <div className='cvButton'>
+                <button
+                  className={`animated-button ${isHovered ? 'hovered' : ''}`}
+                  onMouseEnter={() => setIsHovered(true)}
+                  onMouseLeave={() => setIsHovered(false)}
+                >
+                  My CV
+                </button>
+                {/* <a href=".files/CV_RJ_Paraiso.pdf">My CV</a> */}
+              </div>
+
+              <div className='cvButton'>
+                <button
+                  className={`animated-button ${isHovered2 ? 'hovered' : ''}`}
+                  onMouseEnter={() => setIsHovered2(true)}
+                  onMouseLeave={() => setIsHovered2(false)}
+                >
+                  Websites
+                </button>
+                {/* <a href=".files/CV_RJ_Paraiso.pdf">My CV</a> */}
+              </div>
+
+              <div className='cvButton'>
+                <button
+                  className={`animated-button ${isHovered3 ? 'hovered' : ''}`}
+                  onMouseEnter={() => setIsHovered3(true)}
+                  onMouseLeave={() => setIsHovered3(false)}
+                >
+                  Projects
+                </button>
+                {/* <a href=".files/CV_RJ_Paraiso.pdf">My CV</a> */}
+              </div>
+            </div>
+
+            <div className='description'>
+              Hi!<br></br> My name is RJ.<br></br> Please find your way around my website using the elements provided.
+            </div>
           </div>
         </div>
       </div>
       {/* #### This is the end of the Middle Box */}
+      {/* #### This is the start of the first block  */}
+
+      {/* <div id="block1">
+        <div id="block1Column">
+          <div className='title'>
+            <i>About Me</i>
+          </div>
+
+          <div className='descUnderTitle'>
+            <b>Studying at:</b> Dublin City University<br></br><br></br>
+            <b>From</b>: Philippines<br></br><br></br>
+            <b>Hobbies</b>: Programming, Creative Writing, Learning New Things
+          </div>
+        </div>
+      </div> */}
+
+      {/* #### This is the end of the first block  */}
+      {/* #### This is the start of the footer  */}
+
+      <footer id="footer">
+        <div className='contacts'>
+          <b>Email</b>: rjparaiso369@gmail.com<br></br>
+          <b>Phone</b>: (+353) 089 6108 891
+        </div>
+
+        <div className='other'>
+          Last Updated:
+        </div>
+      </footer>
+      {/* #### This is the end of the footer  */}
 
       {/* #### Dont touch this div */}
     </div>
