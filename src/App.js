436d018f6238f7c59807863b3179bff2f3bc73f5
RJ Paraiso <103213491+ThisIs-RJP@users.noreply.github.com>
2023-11-15 11:53:29 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import insta from './images/instagram.png';
 import linkedIn from './images/linkedIinWhite.png';
 import spotify from './images/spotify.png';
 import discord from './images/discord.png';
+import pfp from './images/pfp.jpg';
 
 // ### Important imports
 import './App.css';
@@ -30,7 +31,7 @@ function App() {
         <div id='contents'>
           <span id="moreContents">
             <a href="" className='links'>About Me</a>
-            <a href="" className='links'>GitHub</a>
+            <a href="https://github.com/thisIs-RJP/" className='links'>GitHub</a>
             <a href="" className='links'>CV</a>
           </span>
         </div>
@@ -48,7 +49,7 @@ function App() {
         {/* #### This is the Middle Box */}
         <div className='aboutMe'>
           <div className='meBox'>
-            <img src={ayase} id="meIcon" />
+            <img src={pfp} id="meIcon" />
             <span id='namePlace2'>RJ Paraiso</span>
             <div id='redLine'></div>
             <div className='profileStuff'>
@@ -56,16 +57,16 @@ function App() {
             </div>
 
             <div className='socials'>
-              <a href="">
+              <a href="https://www.instagram.com/smoodeazy">
                 <img src={insta} className='icons' />
               </a>
-              <a href="">
+              <a href="https://discordapp.com/users/809467129660833843">
                 <img src={discord} className='icons' />
               </a>
-              <a href="">
+              <a href="https://www.linkedin.com/in/rj-paraiso-1910a8289/">
                 <img src={linkedIn} className='icons' />
               </a>
-              <a href="">
+              <a href="https://open.spotify.com/user/22gub3tiz5dxkdspa5neueafy?si=0b24a7df141044ab&nd=1">
                 <img src={spotify} className='icons' />
               </a>
             </div>
@@ -102,19 +103,34 @@ function App() {
               </div>
 
               <div className='cvButton'>
-                <button
-                  className={`animated-button ${isHovered3 ? 'hovered' : ''}`}
-                  onMouseEnter={() => setIsHovered3(true)}
-                  onMouseLeave={() => setIsHovered3(false)}
-                >
-                  Projects
-                </button>
+                <a href="https://github.com/ThisIs-RJP?tab=repositories">
+                  <button
+                    className={`animated-button ${isHovered3 ? 'hovered' : ''}`}
+                    onMouseEnter={() => setIsHovered3(true)}
+                    onMouseLeave={() => setIsHovered3(false)}
+                  >
+                    Projects
+                  </button>
+                </a>
                 {/* <a href=".files/CV_RJ_Paraiso.pdf">My CV</a> */}
               </div>
             </div>
 
             <div className='description'>
-              Hi!<br></br> My name is RJ.<br></br> Please find your way around my website using the elements provided.
+              Hi!<br></br> My name is RJ.<br></br>
+              {/* <div className='container'> */}
+              <div className='leftLine'>
+                Welcome to my website. Please use the elements
+                provided to navigate through it.
+                <br></br>
+
+              </div>
+              {/* </div> */}
+
+              {/* I am a Computer Science student currently in my 2nd year. */}
+
+              {/* "Lorem ipsum dolor sit amet, consectetur adipiscing elit, */}
+              {/* sed do eiusmod tempor incididunt ut labore et dolore magna */}
             </div>
           </div>
         </div>
@@ -145,14 +161,14 @@ function App() {
           <b>Phone</b>: (+353) 089 6108 891
         </div>
 
-        <div className='other'>
+        {/* <div className='other'>
           Last Updated:
-        </div>
+        </div> */}
       </footer>
       {/* #### This is the end of the footer  */}
 
       {/* #### Dont touch this div */}
-    </div>
+    </div >
   );
 }
 
