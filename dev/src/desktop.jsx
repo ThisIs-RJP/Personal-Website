147b48e1346c918efb3d5eca18d4f3ea18db1d23
RJ Paraiso <103213491+ThisIs-RJP@users.noreply.github.com>
2025-03-11 20:28:17 +0000

    feat: Redesign website - Fixed desktop version view (#6)

--- dev/src/desktop.jsx
+++ dev/src/desktop.jsx
@@ -7,6 +7,7 @@ import linkedIn from './images/linkedIinWhite.png';
 import spotify from './images/spotify.png';
 import discord from './images/discord.png';
 import pfp from './images/pfp.jpg';
+import Burger from './images/burger.jpg';
 
 // ### Important imports
 import './App.css';
@@ -18,6 +19,55 @@ import { Link, animateScroll as scroll } from "react-scroll";
 function App() {
   return (
     <div class="desktop-main">
+      <div class="main-box">
+
+        {/* ########################### LEFT BOX CONFIGURATION ########################### */}
+
+        <div class="left-box">
+          <div class="left-box-gap"></div>
+
+          <div class="tab-box">
+            <img src={Burger} />
+          </div>
+
+          <div class="left-box-content-box">
+            <div class="left-box-content-box-gap"></div>
+
+            <div class="left-box-title-box">
+              <div style={{marginLeft: "5%"}}>
+                RJ <br />
+                Paraiso
+              </div>
+            </div>
+
+            <div class="left-box-photo-box">
+              <img src={pfp} />
+            </div>
+
+            <div class="left-box-content-box-gap">
+              <div class="red-line"></div>
+            </div>
+
+            <div class="left-box-short-desc-box">
+              <div>
+
+                Hi! My name is RJ Paraiso. Click on the tabs above for information
+                about me.
+
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+
+        {/* ########################### LEFT BOX CONFIGURATION ########################### */}
+
+
+        <div className='right-box'>
+        </div>
+        
+      </div>
     </div>
   );
 }
